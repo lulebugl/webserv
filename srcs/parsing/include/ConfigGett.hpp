6ef5828428d7227818d81258c1fcff2d66d0fdeb
lilith_exe <jujulie@juju.exe>
2025-07-15 22:09:33 +0200

    COMPILE_GETTER

--- srcs/parsing/include/ConfigGett.hpp
+++ srcs/parsing/include/ConfigGett.hpp
@@ -16,12 +16,13 @@ class Location
 	std::string name;
 	std::map<std::string, std::vector<std::string> > prmtrs; 
 	public:
-	const size_t		getClientMaxBodySize( void )const;
-	const bool			getAutoIndex( void ) const;
-	const std::string*	getRoot( void ) const;
-	const std::string*	getIndex( void ) const;
-	const bool			getMethodIsAllowed(const std::string& method) const;
-	const std::string*	getAlias() const;
+	//TODO
+//	const size_t		getClientMaxBodySize( void )const;
+//	const bool			getAutoIndex( void ) const;
+//	const std::string*	getRoot( void ) const;
+//	const std::string*	getIndex( void ) const;
+//	const bool			getMethodIsAllowed(const std::string& method) const;
+//	const std::string*	getAlias() const;
 
 };
 class Server
@@ -48,36 +49,17 @@ class Server
 class	ConfigGett
 {
 	public:
-//		ConfigGett(const ConfigProcessor& Parser );
-//		virtual ~ConfigGett();
-//
-//
-//        // ♡ Returns a const reference to the main vector of server nodes ♡
-//        const std::vector<const Servers>& getVectorOfServer(void) const;
-//
-//
-//
-//        // ♡ Returns a const reference to the vector containing all configured server ports ♡
-//        const std::vector<int>& getAllPorts() const;
-//
-//        // ♡ Checks whether a given port is present among the configured ports ♡
-//        bool hasPort(int port);
-//		bool getMethod(int port, const std::string& Methods) const;
-//		bool getMethod(int port, const std::string& uri, const std::string& Methods) const;
-//		bool getIsAutoIndex(int port, const std::string& uri, const std::string& Methods)const;
-//		bool getIsAutoIndex(int port, const std::string& Methods)const;
-//		const std::string* getIndex(int port, const)
-//
-//
-//
+		ConfigGett(const ConfigProcessor& Parser );
+		void	CreateServerAndLocation( void )
+		virtual ~ConfigGett();
+
 //		//♡♡♡ Returns the custom error page path for a given port and URI, checking location-level config first.♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡
 //		//♡♡♡ Falls back to server-level config if no match is found in the location.                           ♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡
-//		const std::string*	getErrorPage(int port, const std::string& error, const std::string& uri) const;
+		const std::string*	getErrorPage(int port, const std::string& error, const std::string& uri) const;
 //
 //		//♡♡♡ Returns the custom error page path for a given port, using only server-level configuration.♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡
 //		//♡♡♡ Does not consider URI-specific location blocks.                                            ♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡
-//		const std::string*	getErrorPage(int port, const std::string& error ) const;
-//
+		const std::string*	getErrorPage(int port, const std::string& error ) const;
 
 	private:
 
